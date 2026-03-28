2bc9bd8f6b9f6aa220920fe3fc28b341d1679dd6
laoxuesheng1 <168514034+laoxuesheng1@users.noreply.github.com>
2026-03-28 12:39:21 +0800

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,7 +33,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.siteURL,
-	base: "/",
+	base: "/Mizuki",
 	trailingSlash: "always",
 
 	output: "static",
